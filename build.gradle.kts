203d0d39306ffbdc8b48b86142c12d152fabb42f
Katie Barnett <katiebarnett5@gmail.com>
2025-10-27 20:20:25 +1100

    SDK update to 36

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,20 +11,20 @@ plugins {
 }
 
 ext {
-    //Set the first two digits of the version code to the targetSdkVersion, such as 28.
-    //Set the next three digits to the product version, such as 152 for a product version of 1.5.2.
-    //Set the next two digits to build or release number, such as 01.
-    //Reserve the last two digits for a multi-APK variant, such as 00.
+    // Set the first two digits of the version code to the targetSdkVersion, such as 28.
+    // Set the next three digits to the product version, such as 152 for a product version of 1.5.2.
+    // Set the next two digits to build or release number, such as 01.
+    // Reserve the last two digits for a multi-APK variant, such as 00.
 
     // phone: xx.xxx.xx.00
     // watch: xx.xxx.xx.01
 
-    //35.201.01.00
+    // 35.201.01.00
 
     extra["appVersionName"] = "2.0.3"
     extra["appVersionCode"] = 352030001
-    extra["compileSdk"] = 35
-    extra["targetSdk"] = 33
-    extra["targetSdkMobile"] = 35
+    extra["compileSdk"] = 36
+    extra["targetSdk"] = 36
+    extra["targetSdkMobile"] = 36
     extra["minSdk"] = 26
 }
